e2ff87ef00915a68a77c9b254eaabc29cef2e5fa
Ashad Nadeem Mahmudi <ashadnadeem@gmail.com>
2022-11-03 11:41:17 +0500

    feat: Add some consants
    - fonts
    - colors
    - utils

---
+++ src/constants/colors.ts
@@ -0,0 +1,7 @@
+export const colors = {
+    black: '#000000',
+    grey: '#D9D9D9',
+    white: '#FFFFFF',
+    lightBrown: '#E7DCCA',
+    brown: '#835F2A'
+};
---
+++ src/constants/fonts.ts
@@ -0,0 +1,20 @@
+export const FONT_SIZES = {
+    small: 14,
+    medium: 22,
+    big: 23,
+    large: 25,
+    header: 26,
+    button: 28,
+    title: 35,
+    tag_line: 40,
+    screen_header: 60,
+    logo_large: 140
+};
+
+export const FONT_STYLES = {
+    title_bold_font: 'irish_grover_bold',
+    base_font: 'irish_grover',
+    light_font: 'irish_grover_light',
+    bold_font: 'inter_bold',
+    italic_font: 'inter_italic',
+};
