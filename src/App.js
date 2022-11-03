25456afbb839eb1aaa5f6a4724fac6c9e17a4d54
maheensabahat <m.sabahat_19748@khi.iba.edu.pk>
2022-11-04 00:02:56 +0500

    feat: Style Login/Register page background

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import './App.css';
 import LoginForm from './components/LoginForm';
-import background from './assets/background - login.jpg';
 
 function App() {
   return (
     <div className="App">
-      <div className='ImageBG'>
-        <img src={background} alt="background" height={'100%'} width= {'100%'}/>;
+      <div className='LoginPage'>
+
+        <div className='ImageBG'>
+          <div class="ImageOverlay"></div>
+        </div>
+
+        <div class= 'Login'>
+          {/* <LoginForm/> */}
+        </div>
+
       </div>
-      {/* <LoginForm/> */}
     </div>
   );
 }
