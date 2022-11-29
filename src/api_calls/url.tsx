5ea8bde9f0995dc98406297fc4050d789ea2a918
abc <khaulah.ahmed@gmail.com>
2022-11-30 01:45:39 +0500

    product screen fixes

--- src/api_calls/url.tsx
+++
@@ -1,3 +0,0 @@
-const BaseURL = " https://d31f-103-244-178-160.ap.ngrok.io";
-
-export default BaseURL;
--- src/components/ProductDescription/index.tsx
+++ src/components/ProductDescription/index.tsx
@@ -9,17 +9,17 @@ import './styles.css';
 const ProductDesc: FC = () => {
     return (
         <>
-            <div className="page">
-                <div className="content">
-                    <h1 className="title">  Five Rupee Note</h1>
-                    <div className="description"> Product desc: Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.</div>
-                    <div className="collection"> Collection: Collectibles </div>
-                    <div className="price"> $9.99 </div>
-                    <div className="description2">Highest Bid: $ 9.99</div>
-                    <div className="button">
+            <div className="desc_page">
+                <div className="desc_content">
+                    <h1 className="desc_title">  Five Rupee Note</h1>
+                    <div className="desc_description"> Product desc: Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.</div>
+                    <div className="desc_collection"> Collection: Collectibles </div>
+                    <div className="desc_price"> $9.99 </div>
+                    <div className="desc_description2">Highest Bid: $ 9.99</div>
+                    <div className="desc_button">
                         <WhiteRectangleArrowButton text="Add to Cart" onPress={() => { }} ></WhiteRectangleArrowButton>
                     </div>
-                    <div className="button">
+                    <div className="desc_button">
                         <RectangleArrowButton text="Bid on it" invert={false} onPress={() => { }} ></RectangleArrowButton>
                     </div>
                 </div>
