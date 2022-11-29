c693bf70e90966d0bee36861e012f5ccf81c892b
Ashad Nadeem Mahmudi <ashadnadeem@gmail.com>
2022-11-29 16:05:19 +0500

    feat: navigation on header

--- src/components/Headers/DefaultHeader/index.tsx
+++ src/components/Headers/DefaultHeader/index.tsx
@@ -1,6 +1,7 @@
 import { Person, ShoppingCart } from "@mui/icons-material";
 import { Button as h1, IconButton } from "@mui/material";
 import React, { FC } from "react";
+import { useNavigate } from "react-router-dom";
 
 import "./styles.css";
 
@@ -9,7 +10,11 @@ type Props = {
 }
 
 const DefaultHeader: FC<Props> = ({inverted}) => {
-    console.log(inverted);
+    const navigate = useNavigate();
+    const navigateToAboutPage = () => navigate('/about');
+    const navigateToCollectionPage = () => navigate('/collection');
+    const navigateToAuctionPage = () => navigate('/auction');
+    const navigateToFavouritePage = () => navigate('/favourite');
     const onSellItemClick = () => {
         console.log("Sell item clicked");
     }
@@ -18,10 +23,10 @@ const DefaultHeader: FC<Props> = ({inverted}) => {
             <label className={inverted?"default_header_logo_invert":"default_header_logo"}>Logo</label>
             <div className={inverted?"default_header_menu_invert":"default_header_menu"}>
                 {/* Maerial UI default_header_text */}
-                <h1 className={inverted?"default_header_text_invert":"default_header_text"} color="inherit" onClick={onSellItemClick}>About</h1>
-                <h1 className={inverted?"default_header_text_invert":"default_header_text"} color="inherit">Collections</h1>
-                <h1 className={inverted?"default_header_text_invert":"default_header_text"} color="inherit">Auctions</h1>
-                <h1 className={inverted?"default_header_text_invert":"default_header_text"} color="inherit">Favourites</h1>
+                <h1 className={inverted?"default_header_text_invert":"default_header_text"} color="inherit" onClick={navigateToAboutPage}>About</h1>
+                <h1 className={inverted?"default_header_text_invert":"default_header_text"} color="inherit" onClick={navigateToCollectionPage} >Collections</h1>
+                <h1 className={inverted?"default_header_text_invert":"default_header_text"} color="inherit" onClick={navigateToAuctionPage}>Auctions</h1>
+                <h1 className={inverted?"default_header_text_invert":"default_header_text"} color="inherit" onClick={navigateToFavouritePage}>Favourites</h1>
                 <label className={inverted?"default_header_raised_button_invert":"default_header_raised_button"} onClick={onSellItemClick}>Sell Items</label>
                 <IconButton className='default_header_icon' aria-label="account" color='inherit' size="medium">
                     <Person />
